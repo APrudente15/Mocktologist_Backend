29d7a7dad8c261d4fe4f4b6d3c804a1ad8df1408
henrietta adeniran <henrietta.akindele@gmail.com>
2024-03-18 17:13:27 +0000

    add user models

--- models/user.js
+++ models/user.js
@@ -1,1 +1,51 @@
-// User Model
+const db = require("../database/connect");
+
+
+class User {
+    constructor ({ user_id, fname, lname, email, password, vegan }) {
+        this.id = user_id;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.password = password;
+        this.vegan = vegan;
+    }
+
+    static async getAll() {
+        const response = await db.query("SELECT * FROM userAccount;");
+        return response.rows.map(g => new User(g));
+    }
+
+    static async getOneById(id) {
+        const response = await db.query("SELECT user_id, fname, lname, email, password, vegan FROM userAccount WHERE user_id = $1;", [id]);
+        if (response.rows.length != 1) {
+            throw new Error("Unable to find user.");
+        };
+        return new User(response.rows[0]);
+    }
+
+    static async create(body) {
+        const {fname, lname, email, password, vegan} = body;
+        const response = await db.query('INSERT INTO userAccount (fname, lname, email, password, vegan) VALUES ($1, $2, $3, $4, $5) RETURNING *;', [fname, lname, email, password, vegan]);
+    
+        return new User(response.rows[0]);
+    }
+
+    async update(body) {
+        const {fname, lname, email, password, vegan} = body;
+    
+        if (!fname || !lname, !email || !password || !vegan) {
+            throw new Error("Missing Data!");
+        };
+        const response = await db.query('UPDATE userAccount SET fname = $1, lname = $2, email = $3, password = $4, vegan = $5 WHERE user_id = $6 RETURNING *;', [fname, lname, email, password, vegan, this.id]);
+        return new User(response.rows[0]);
+    }
+
+    async destroy() {
+        const response = await db.query("DELETE FROM userAccount WHERE user_id = $1 RETURNING *;", [this.id]);
+        return new User(response.rows[0]);
+    }
+
+}
+
+module.exports = User;
