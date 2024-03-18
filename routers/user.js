bdf774707c3029048fe8b08728ae207ceef6f9a0
henrietta adeniran <henrietta.akindele@gmail.com>
2024-03-18 16:11:25 +0000

    create user routes

--- routers/user.js
+++ routers/user.js
@@ -1,1 +1,17 @@
-// User Router
+const { Router } = require('express');
+
+const userController = require('../controllers/user.js');
+const User = require('../models/user.js');
+
+const userRouter = Router();
+
+userRouter.get("/", userController.index);
+userRouter.post("/register", userController.register);
+userRouter.post("/login", userController.login);
+userRouter.patch("/:id", userController.update);
+userRouter.delete("/:logout", userController.destroy);
+userRouter.get("/:token", userController.showToken);
+userRouter.get("/count/:id", userController.showCount);
+
+
+module.exports = userRouter;
