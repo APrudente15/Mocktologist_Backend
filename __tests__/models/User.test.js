f7166a6261db67cd7ef0ba053f1f4613760586b8
APrudente15 <110691505+APrudente15@users.noreply.github.com>
2024-03-21 22:14:31 +0000

    add more tests in user model

--- __tests__/models/User.test.js
+++ __tests__/models/User.test.js
@@ -60,3 +60,151 @@ describe("User", () => {
     });
   });
 });
+
+describe("getOneById", () => {
+  it("gets one user on successful db query", async () => {
+    let testUser = {
+      fname: "Angelika",
+      lname: "Prudente",
+      email: "testEmail",
+      password: "1234",
+      vegan: false,
+      image: "img7",
+    };
+
+    jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [testUser] });
+
+    const result = await User.getOneById(1);
+
+    expect(result).toEqual(testUser);
+  });
+
+  it("should throw an Error on db query error", async () => {
+    jest.spyOn(db, "query").mockRejectedValueOnce(new Error("Unable to find user."));
+
+    try {
+      await User.getOneById(1);
+    } catch (error) {
+      expect(error).toBeTruthy();
+      expect(error.message).toBe("Unable to find user.");
+    }
+  });
+});
+
+describe('create', () => {
+  it('creates a user on successful db query', async () => {
+    let userData = {       
+    fname: "Riri",
+    lname: "Smith",
+    email: "testEmail",
+    password: "4321",
+    vegan: true,
+    image: "img8",}
+    jest.spyOn(db, 'query')
+      .mockResolvedValueOnce({ rows: [{ ...userData, id: 1 }] });
+
+    const result = await User.create(userData);
+    
+    expect(result).toBeTruthy()
+    expect(result).toHaveProperty('fname')
+    expect(result).toHaveProperty('lname')
+    expect(result).toHaveProperty('email')
+    expect(result).toHaveProperty('password')
+    expect(result).toHaveProperty('vegan')
+    expect(result).toHaveProperty('image')
+  })
+
+  // it('should throw an Error on db query error', async () => {
+  //   try {
+  //     await User.create({ fname: "sugarPlum" })
+  //   } catch (error) {
+  //     expect(error).toBeTruthy()
+  //     expect(error.message).toBe('age is missing')
+  //   }
+  // })
+})
+
+describe('update', () => {
+  it('should return the updated user', async () => {
+    const user = new User({      
+      fname: "Sugar",
+      lname: "Plum",
+      email: "testEmail",
+      password: "9876",
+      vegan: true,
+      image: "img10",})
+    jest.spyOn(db, 'query')
+      .mockResolvedValueOnce({ rows: [{ fname: "Plum",
+      lname: "Sugar",
+      email: "testEmail1",
+      password: "7373",
+      vegan: true,
+      image: "img11" }] })
+
+    const result = await user.update({fname: "Plum",
+    lname: "Sugar",
+    email: "testEmail1",
+    password: "7373",
+    vegan: true,
+    image: "img11" })
+
+    expect(result).toBeInstanceOf(User)
+    expect(result.fname).toBe('Plum')
+    expect(result.lname).toBe('Sugar')
+    expect(result.email).toBe('testEmail1')
+    expect(result.password).toBe('7373')
+    expect(result.vegan).toBe(true)
+    expect(result.image).toBe('img11')
+    expect(result).not.toEqual(user)
+  })
+
+  // it('should throw an error if first name is missing', async () => {
+  //   try {
+  //     const user = new User({ fname: "Plum",
+  //     lname: "Sugar",
+  //     email: "testEmail1",
+  //     password: "7373",
+  //     vegan: true,
+  //     image: "img11" })
+  //     await user.update({ 
+  //     lname: 'puppet', 
+  //     email: "testEmail1",
+  //     password: "7373",
+  //     vegan: true,
+  //     image: "img11" });
+  //   } catch (error) {
+  //     expect(error).toBeTruthy()
+  //     expect(error.message).toBe('Missing Data!')
+  // }
+})
+//})
+
+describe("getOneByEmail", () => {
+  it("gets one user by email on successful db query", async () => {
+    let testUser = {
+      fname: "Angelika",
+      lname: "Prudente",
+      email: "testEmail",
+      password: "1234",
+      vegan: false,
+      image: "img7",
+    };
+
+    jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [testUser] });
+
+    const result = await User.getOneByEmail(1);
+
+    expect(result).toEqual(testUser);
+  });
+
+  it("should throw an Error on db query error", async () => {
+    jest.spyOn(db, "query").mockRejectedValueOnce(new Error("Unable to find user."));
+
+    try {
+      await User.getOneByEmail(1);
+    } catch (error) {
+      expect(error).toBeTruthy();
+      expect(error.message).toBe("Unable to find user.");
+    }
+  });
+});
