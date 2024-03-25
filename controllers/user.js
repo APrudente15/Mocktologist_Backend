330f8433fef7237b14e320d1e2e1e92ba8cb2bf3
genuisj01 <genuisj01@gmail.com>
2024-03-25 11:18:20 +0000

    Remove nutritional info, get share route

--- controllers/user.js
+++ controllers/user.js
@@ -90,6 +90,16 @@ async function destroy (req, res) {
     }
 }
 
+async function getShare (req, res) {
+    try {
+        const id = parseInt(req.params.id);
+        const user = await User.getOneById(id);
+        res.status(200).json({"fname": user.fname});
+    } catch (err) {
+        res.status(404).json({"error": err.message});
+    }
+}
+
 module.exports = {
-    index, register, update, destroy, login, showToken, showCount
+    index, register, update, destroy, login, showToken, showCount, getShare
 }
